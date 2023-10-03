4c2f4a068715f528c46688041d42160dfe05a16c
Hill <125409221+NARI1108@users.noreply.github.com>
2023-10-03 14:26:45 +0330

    We use the typeface class to set the desired font for widgets.

--- app/src/main/java/com/example/bilingualbook/MainActivity.java
+++ app/src/main/java/com/example/bilingualbook/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.bilingualbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txt_title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        typeFace();
+    }
+    public void findViews(){
+        txt_title = findViewById(R.id.txt_title);
+    }
+    //We use the typeface class to set the desired font for widgets.
+    public void typeFace(){
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"font/brush.TTF");
+        txt_title.setTypeface(typeface);
     }
 }
