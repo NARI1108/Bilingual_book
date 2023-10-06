63ec22611abf73c34748f6c3a716719a28f5f1d8
Hill <125409221+NARI1108@users.noreply.github.com>
2023-10-06 13:07:41 +0330

    Adding a few text to strings.xml and Addressing the texts of the strings.xml to the main activity.

--- app/src/main/java/com/example/bilingualbook/MainActivity.java
+++ app/src/main/java/com/example/bilingualbook/MainActivity.java
@@ -32,9 +32,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu (Menu menu){
         //Creating menu option items.
-        menu.add(0, 1, 1, "about us");
-        menu.add(0, 2, 2, "change Language");
-        menu.add(0, 3, 3, "guide Book");
+        menu.add(0, 1, 1,getResources().getString(R.string.about_us));
+        menu.add(0, 2, 2,getResources().getString(R.string.change_lan));
+        menu.add(0, 3, 3,getResources().getString(R.string.guide_book));
         return super.onCreateOptionsMenu(menu);
     }
     //The click method on each of the menu items.
@@ -44,13 +44,13 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
         //By typing a conditional switch case command, we specify what to do by clicking on each item.
         switch (id){
-            case 1: new AlertDialog.Builder(this).setTitle("about us").setMessage("hi world").show();
+            case 1: new AlertDialog.Builder(this).setTitle("about us").setMessage(getResources().getString(R.string.hello_world)).show();
                 break;
             case 2:
-                Toast.makeText(getApplicationContext(), "changing ur Language", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),getResources().getString(R.string.change_lan), Toast.LENGTH_SHORT).show();
                 break;
             case 3:
-                Toast.makeText(getApplicationContext(), "our Guide of Book", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),getResources().getString(R.string.guide_book), Toast.LENGTH_SHORT).show();
                 break;
         }
         return super.onOptionsItemSelected(item);
